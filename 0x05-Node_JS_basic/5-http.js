a4064c4609754ef999f21fde1d60b0041970931c
NomatheLuckyOne <thamiemathebula@gmail.com>
2023-06-26 18:26:17 +0000

    fixed check errors

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,25 +1,26 @@
-const server = require('http');
-const host = require('127.0.0.1');
-const file = require('./3-read_file_async');
-
+const http = require('http');
+const students = require('./3-read_file_async');
+const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = server.creatServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
     res.end('Hello Holberton School!');
-  } else if (req.url === '/students') res.write('This is the list of our students\n');
-	  file(process.argv[2]).then((data) => {
-	  	res.write(`Number of students: ${data.students.length}\n`);
-    res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
-    res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.csStudents.join(', ')}\n`);
-    res.end();
-	  }).catch((err) => res.end(err.message));
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    students(process.argv[2]).then((data) => {
+      res.write(`Number of students: ${data.students.length}\n`);
+      res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
+      res.end();
+    }).catch((err) => res.end(err.message));
+  }
 });
-
-app.listen(port, host, () => {
-  console.log(`Server running at http://${host}:${port}`);
+  
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
 });
 
 module.exports = app;
