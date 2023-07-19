4194608831683989bbbfa789d80b18fdfd93e7ad
NomatheLuckyOne <thamiemathebula@gmail.com>
2023-07-19 20:30:42 +0000

    fixed task6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,14 +1,14 @@
 const { expect } = require('chai');
 
-const getPaymentTokenFromAPI = require('./6-payment_token');
+const getPaymentTokenFromApi = require('./6-payment_token');
 
-describe('getPaymentTokenFromAPI', () => {
-  it('checks output of getPaymentTokenFromAPI with true as success', () => new Promise((done) => {
-    getPaymentTokenFromAPI(true)
-      .then((resp) => {
-        expect(resp).to.include({ data: 'Successful response from the API' });
+describe('getPaymentTokenFromApi', () => {
+  it('checks output of getPaymentTokenFromApi with true as success', (done) => {
+    getPaymentTokenFromApi(true)
+      .then((res) => {
+        expect(res).to.include({ data: 'Successful response from the API' });
         done();
       })
       .catch((err) => done(err));
-  }));
+  });
 });
